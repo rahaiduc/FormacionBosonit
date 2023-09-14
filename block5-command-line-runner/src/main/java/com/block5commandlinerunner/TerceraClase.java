f10973611f57d80ace6d6f618cc5bf65eb4465ca
rahaiduc <rahaiduc@unirioja.es>
2023-09-14 13:15:14 +0200

    B5PROPIEDADES

--- block5-command-line-runner/src/main/java/com/block5commandlinerunner/TerceraClase.java
+++ block5-command-line-runner/src/main/java/com/block5commandlinerunner/TerceraClase.java
@@ -6,8 +6,9 @@ import org.springframework.stereotype.Service;
 @Service
 public class TerceraClase implements CommandLineRunner {
 
+
     @Override
-    public void run(String... args) throws Exception {
+    public void run( String... args) throws Exception {
         System.out.println("Soy la tercera clase");
     }
 }

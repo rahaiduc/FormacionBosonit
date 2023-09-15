606ebcbd5b76b22e65c74c66d1333def090b41ca
rahaiduc <rahaiduc@unirioja.es>
2023-09-15 10:56:29 +0200

    B5PROPIEDADES

--- block5-properties/src/main/java/com/block5properties/BeanPropiedades.java
+++ block5-properties/src/main/java/com/block5properties/BeanPropiedades.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Component;
 import org.springframework.boot.CommandLineRunner;
 @Component
@@ -15,10 +16,17 @@ public class BeanPropiedades implements CommandLineRunner{
 
     @Value("${my.number}")
     private String Number;
+
+    @Value("${NEW_PROPERTY}")
     private String newProperty;
 
-    BeanPropiedades(@Value("${new.property:new property no tiene valor}") String newProperty){
-        this.newProperty=newProperty;
+    /*@Value("${MYURL}")
+    private String myurl;*/
+
+    @Value("${MYURL2:NO_tengo_valor}")
+    private String myurl2;
+    BeanPropiedades(){
+
     }
 
     @Override
@@ -26,5 +34,7 @@ public class BeanPropiedades implements CommandLineRunner{
         System.out.println("El valor de greeting es: " + this.greeting);
         System.out.println("El valor de my.number es: " + this.Number);
         System.out.println("El valor de new.property es: " + this.newProperty);
+        //System.out.println("El valor de myurl2 es: " + this.myurl);
+        System.out.println("El valor de myurl2 es: " + this.myurl2);
     }
 }

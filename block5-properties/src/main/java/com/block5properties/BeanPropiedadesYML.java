c9ecd614ac4714c904cc0c420639601d3a4b6eb4
rahaiduc <rahaiduc@unirioja.es>
2023-09-15 12:21:39 +0200

    Acabado parte propiedades

--- block5-properties/src/main/java/com/block5properties/BeanPropiedadesYML.java
+++ block5-properties/src/main/java/com/block5properties/BeanPropiedadesYML.java
@@ -1,17 +1,16 @@
 package com.block5properties;
 
 import lombok.NoArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
-import org.springframework.core.env.Environment;
-import org.springframework.stereotype.Component;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.stereotype.Component;
+
 @Component
 @NoArgsConstructor
-public class BeanPropiedades implements CommandLineRunner{
+@PropertySource("classpath:application.yml")
+public class BeanPropiedadesYML implements CommandLineRunner {
 
     @Value("${greeting}")
     private String greeting;
@@ -22,19 +21,17 @@ public class BeanPropiedades implements CommandLineRunner{
     @Value("${NEW_PROPERTY}")
     private String newProperty;
 
-    /*@Value("${MYURL: No_existe}")*/
+    @Value("${MYURL: No_existe}")
     private String myurl;
 
     @Value("${MYURL2:NO_tengo_valor}")
     private String myurl2;
 
-    @Autowired
-    private Environment env;
+
 
     @Override
     public void run(String... args) throws Exception {
-        this.myurl=env.getProperty("MYURL");
-        System.out.println("-----------LEYENDO PROPIEDADES DESDE APPLICATION.PROPERTIES--------------");
+        System.out.println("-----------LEYENDO PROPIEDADES DESDE YAML--------------");
         System.out.println("El valor de greeting es: " + this.greeting);
         System.out.println("El valor de my.number es: " + this.Number);
         System.out.println("El valor de new.property es: " + this.newProperty);

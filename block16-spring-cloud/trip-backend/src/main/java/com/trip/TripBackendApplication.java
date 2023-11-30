0fecb2b71d589973671ff2ba267b4481cd43b4c0
rahaiduc <rahaiduc@unirioja.es>
2023-11-30 10:49:49 +0100

    Bloque Spring Cloud

---
+++ block16-spring-cloud/trip-backend/src/main/java/com/trip/TripBackendApplication.java
@@ -0,0 +1,13 @@
+package com.trip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TripBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TripBackendApplication.class, args);
+    }
+
+}
